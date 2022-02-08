ba11c121f14857cff72e5fe12861fce74c02c766
ivrolan <ivanlpzbrc01@gmail.com>
2022-02-08 10:50:19 +0100

    angle fixed to take the laser at the right and back. Style fixed

--- include/follow-wall-l4ros2/SensingNode.hpp
+++ include/follow-wall-l4ros2/SensingNode.hpp
@@ -1,3 +1,7 @@
+#ifndef FOLLOW_WALL_L4ROS2_INCLUDE_FOLLOW_WALL_L4ROS2_SENSINGNODE_HPP_
+#define FOLLOW_WALL_L4ROS2_INCLUDE_FOLLOW_WALL_L4ROS2_SENSINGNODE_HPP_
+// Copyright 2022 <@ivrolan>
+#include <string>
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "example_interfaces/msg/int8_multi_array.hpp"
@@ -11,15 +15,15 @@ enum{
 #define SAFE_DISTANCE 0.4
 #define INTERVAL 0.05
 
-class SensingNode : public rclcpp::Node
-{
-    public:
-        SensingNode(const std::string& name);
+class SensingNode : public rclcpp::Node {
+ public:
+    explicit SensingNode(const std::string& name);
 
-    private:
-        void callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) const;
+ private:
+    void callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) const;
 
-    private:
-        rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub_;
-        rclcpp::Publisher<example_interfaces::msg::Int8MultiArray>::SharedPtr pub_;
+ private:
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub_;
+    rclcpp::Publisher<example_interfaces::msg::Int8MultiArray>::SharedPtr pub_;
 };
+#endif  // FOLLOW_WALL_L4ROS2_INCLUDE_FOLLOW_WALL_L4ROS2_SENSINGNODE_HPP_
